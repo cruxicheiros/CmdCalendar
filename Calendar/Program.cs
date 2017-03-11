32741d0f1390370d2a604ae98f5ad27c17afc00a
cruxicheiros <hazel.seanor@gmail.com>
2017-03-11 17:34:23 +0000

    Add project files.

---
+++ Calendar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calendar
+{
+    class Event
+    {
+        public string Label;
+        public DateTime StartTime;
+
+        public Event(string Label, DateTime StartTime)
+        {
+            this.Label = Label;
+            this.StartTime = StartTime;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event Birthday = new Event("Happy Birthday", DateTime.Today);
+
+            Console.WriteLine(Birthday.Label + DateTime.Today);
+        }
+
+    }
+}
