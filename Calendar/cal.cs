33b0f4a4813e249f500c79f63a917cd8b94cc67e
cruxicheiros <hazel.seanor@gmail.com>
2017-03-12 00:59:03 +0000

    I/O to and from an XML file to store all your events in works now.

--- Calendar/cal.cs
+++ Calendar/cal.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace Calendar
 {
-    class Event
+    public class Event
     {
         public string Label;
         public DateTime StartTime;
@@ -13,15 +15,37 @@ namespace Calendar
             this.Label = Label;
             this.StartTime = StartTime;
         }
-
+        private Event() : this(null, DateTime.MinValue) { }
     }
 
     class Cal
     {
+        static void Serialize(List<Event> events)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Event>));
+            StreamWriter OutputFile = new StreamWriter(File.OpenWrite("cal_events.xml"));
+            ser.Serialize(OutputFile, events);
+            OutputFile.Dispose();
+
+        }
+
+        static List<Event> Deserialize()
+        {
+            List<Event> CalEvents;
+
+            XmlSerializer ser = new XmlSerializer(typeof(List<Event>));
+            StreamReader InputFile = new StreamReader(File.OpenRead("cal_events.xml"));
+            CalEvents = (List<Event>)ser.Deserialize(InputFile);
+            InputFile.Dispose();
+
+            return CalEvents;
+        }
+
         static void Main(string[] args)
         {
             Event OperatingEvent = new Event("", DateTime.Today);
-            Console.WriteLine(args.Length);
+            List<Event> CalendarEvents = Deserialize();
+            
             if (args.Length == 0)
             {
                 Console.WriteLine("Please enter an argument.");
@@ -29,26 +53,38 @@ namespace Calendar
             else
             {
                 if (args[0] == "--add-event")
-                    if (args.Length == 3) {
+                    if (args.Length == 3)
+                    {
                         {
                             try
                             {
                                 OperatingEvent.StartTime = Convert.ToDateTime(args[1]);
                                 OperatingEvent.Label = args[2];
 
-                            } catch (FormatException)
+                            }
+                            catch (FormatException)
                             {
                                 Console.WriteLine("Please provide your second argument in the format YYYY/MM/DD");
                             }
 
                         }
 
-                        Console.WriteLine(OperatingEvent.Label);
-                    } else
+                        if (OperatingEvent.Label == "")
+                        {
+                            Console.WriteLine("Can't have an event with an empty label.");
+                        }
+                        else
+                        {
+                            CalendarEvents.Add(OperatingEvent);
+                            Console.WriteLine("Event \"{0}\" added on day {1}", OperatingEvent.Label, OperatingEvent.StartTime);
+
+                            Serialize(CalendarEvents);
+                        }
+                    }
+                    else
                     {
                         Console.WriteLine("Arguments should be provided in the form: cal --add-event [date (format: YYYY/MM/DD)] [label (in \"quotation marks\")]");
                     }
-
 
             }
             //Event Birthday = new Event("Happy Birthday", DateTime.Today);
